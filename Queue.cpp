b3d621464078328c664dba1cd5c4cc3a8240db36
Procopii Maria <77497709+MariaProcopii@users.noreply.github.com>
2022-03-12 11:25:14 +0200

    Update Queue.cpp

--- Queue.cpp
+++ Queue.cpp
@@ -53,3 +53,58 @@ int main() {
 	printList();
 	return 0;
 }
+/*include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+	int data;
+	struct node* next;
+};
+void enqueue(struct node** front, struct node** rear, int data) {
+	struct node* new_node = (struct node*)malloc(sizeof(struct node));
+	new_node->data = data;
+	new_node->next = NULL;
+	if (*front == NULL && *rear == NULL) {
+		*front = *rear = new_node;
+		return;
+	}
+	else {
+		(*rear)->next = new_node;
+		*rear = new_node;
+	}
+}
+
+void dequeue(struct node** front, struct node** rear) {
+	struct node* temp = *front;
+	if (front == NULL) {
+		printf("List is empty\n");
+		return;
+	}
+	else {
+		*front = (*front)->next;
+		if (front == NULL) {
+			rear = NULL;
+		}
+		free(temp);
+	}
+}
+
+void printList(struct node** front) {
+	struct node* temp = *front;
+	while (temp != NULL) {
+		printf("%d\n", temp->data);
+		temp = temp->next;
+	}
+}
+int main() {
+	struct node* front = NULL;
+	struct node* rear = NULL;
+	enqueue(&front, &rear, 1);
+	enqueue(&front, &rear, 2);
+	enqueue(&front, &rear, 3);
+	printList(&front);
+	printf("After dequeue:\n");
+	dequeue(&front, &rear);
+	printList(&front);
+	return 0;
+}*/
